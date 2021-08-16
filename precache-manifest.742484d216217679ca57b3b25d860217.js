c8f0bbf5e93c41804d3c38fdec0f86097ee76cce
jdim21 <jdim21@gmail.com>
2021-08-16 01:39:46 -0400

    init

---
+++ precache-manifest.742484d216217679ca57b3b25d860217.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ee7b1d37a08e20e2a3e53d3708548e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "168a47c96386df835beb",
+    "url": "/static/css/2.06ebec9a.chunk.css"
+  },
+  {
+    "revision": "5a052a9dea182cda0235",
+    "url": "/static/css/main.a2687636.chunk.css"
+  },
+  {
+    "revision": "168a47c96386df835beb",
+    "url": "/static/js/2.a706c6d2.chunk.js"
+  },
+  {
+    "revision": "d2e09aded9686c343e34a065031febca",
+    "url": "/static/js/2.a706c6d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a052a9dea182cda0235",
+    "url": "/static/js/main.493f525f.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  }
+]);
